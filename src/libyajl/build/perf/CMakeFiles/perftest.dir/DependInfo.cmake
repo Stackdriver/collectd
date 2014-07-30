9302c7e247f3f1eb02b479108f236772dfd9ff46
zhihua wen <zhihuawen@google.com>
2014-07-30 16:03:20 -0700

    add the new write_gcm plugin

---
+++ src/libyajl/build/perf/CMakeFiles/perftest.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/usr/local/google/home/zhihuawen/git/collectd-gcm-sd/src/libyajl/perf/documents.c" "/usr/local/google/home/zhihuawen/git/collectd-gcm-sd/src/libyajl/build/perf/CMakeFiles/perftest.dir/documents.c.o"
+  "/usr/local/google/home/zhihuawen/git/collectd-gcm-sd/src/libyajl/perf/perftest.c" "/usr/local/google/home/zhihuawen/git/collectd-gcm-sd/src/libyajl/build/perf/CMakeFiles/perftest.dir/perftest.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/usr/local/google/home/zhihuawen/git/collectd-gcm-sd/src/libyajl/build/src/CMakeFiles/yajl_s.dir/DependInfo.cmake"
+  )
---
+++ src/libyajl/build/src/CMakeFiles/yajl_s.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+FILE(REMOVE_RECURSE
+  "../yajl-2.1.1/lib/libyajl_s.a"
+)
