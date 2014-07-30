bacbd860c0cdd3a45e083b2a287c864562d8c228
zhihua wen <zhihuawen@google.com>
2014-07-30 16:03:58 -0700

    add auto files

--- libltdl/config.h
+++ libltdl/config.h
@@ -120,7 +120,7 @@
 /* #undef LTDL_DLOPEN_DEPLIBS */
 
 /* Define to the system default library search path. */
-#define LT_DLSEARCH_PATH "/lib64:/usr/lib64:/lib:/usr/lib:/usr/lib64/atlas:/usr/lib64/iscsi:/usr/lib64/llvm:/usr/lib64/mysql:/usr/lib64/qt-3.3/lib:/usr/lib64/tracker-0.14:/usr/lib64/xulrunner"
+#define LT_DLSEARCH_PATH "/lib:/usr/lib:/usr/lib/x86_64-linux-gnu/fakechroot:/lib/i386-linux-gnu:/usr/lib/i386-linux-gnu:/lib/i686-linux-gnu:/usr/lib/i686-linux-gnu:/usr/local/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:/usr/lib/nvidia-331:/usr/lib32/nvidia-331:/lib32:/usr/lib32"
 
 /* The archive extension */
 #define LT_LIBEXT "a"
