379564ed7226d02a31f67611e1b1403dba782b48
Igor Peshansky <igorp@google.com>
2019-09-30 11:42:35 -0400

    Fix the format truncation warnings.

--- src/utils_db_query.c
+++ src/utils_db_query.c
@@ -265,7 +265,7 @@ static int udb_result_submit (udb_result_t *r, /* {{{ */
     }
     else
     {
-      char tmp[DATA_MAX_NAME_LEN];
+      char tmp[DATA_MAX_NAME_LEN - 10];
 
       int status = strjoin (tmp, sizeof (tmp), r_area->instances_buffer,
           r->instances_num, "-");
