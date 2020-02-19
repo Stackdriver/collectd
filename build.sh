40227fa0e2b2aeafe76fc31907f5794f34487c83
Igor Peshansky <igorp@google.com>
2020-02-19 01:06:57 +0000

    Guard the make install step to allow disabling it in the script.

--- build.sh
+++ build.sh
@@ -190,13 +190,16 @@ if test "${WINDOWS}" = "yes"; then
 	echo "#include <config.h.in>" >> src/gnulib_config.h
 
 	make
-	make install
 
-	cp ".libs/libcollectd-0.dll" "${INSTALL_DIR}"
-	cp "${GNULIB_DIR}/libgnu.dll" "${INSTALL_DIR}"
-	cp "${MINGW_ROOT}/bin/zlib1.dll" "${INSTALL_DIR}"
-	cp "${MINGW_ROOT}/bin/libwinpthread-1.dll" "${INSTALL_DIR}"
-	cp "${MINGW_ROOT}/bin/libdl.dll" "${INSTALL_DIR}"
+	if test "${NOINSTALL:-no}" != yes; then
+		make install
+
+		cp ".libs/libcollectd-0.dll" "${INSTALL_DIR}"
+		cp "${GNULIB_DIR}/libgnu.dll" "${INSTALL_DIR}"
+		cp "${MINGW_ROOT}/bin/zlib1.dll" "${INSTALL_DIR}"
+		cp "${MINGW_ROOT}/bin/libwinpthread-1.dll" "${INSTALL_DIR}"
+		cp "${MINGW_ROOT}/bin/libdl.dll" "${INSTALL_DIR}"
+	fi
 
 	echo "Done"
 fi
